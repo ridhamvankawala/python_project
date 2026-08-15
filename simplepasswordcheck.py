a100a534368e6c0e9f8aad8bec1d1671b31062f9
Ridham Vankawala <ridhamvankawala@gmail.com>
2026-08-15 16:45:44 +0530

    Delete simplepasswordcheck.py

--- simplepasswordcheck.py
+++
@@ -1,7 +0,0 @@
-correct=123456
-name=input("enter name=")
-password=int(input("enter password="))
-if password==correct:
-    print("password is correct")
-else:
-    print("password is not correct")
